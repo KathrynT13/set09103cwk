6b81fb3c33f5c69a88ff715b5e12bcb65bd9a934
KathrynT13 <kathrynjthomson@gmail.com>
2021-11-18 09:35:43 +0000

    edited directories

--- root.py
+++ root.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('usertest.html')
+    return render_template('users/usertest.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
