652327523ed12949db0290728e49482acc688a73
KathrynT13 <kathrynjthomson@gmail.com>
2021-11-18 11:59:30 +0000

    added other user files, root directory and following page; added navbar functionality

--- root.py
+++ root.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def root():
-    return render_template('users/usertest.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/me')
+def me():
+    return render_template('users/mine.html')
+
+@app.route('/friends')
+def friends():
+    return render_template('friends.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
