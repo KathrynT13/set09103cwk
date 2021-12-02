70673c93731253ffd95241514dbbd55c84eb7c35
KathrynT13 <kathrynjthomson@gmail.com>
2021-12-02 10:39:09 +0000

    created post prototype

--- root.py
+++ root.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,6 +24,20 @@ def cats():
 @app.route('/walkingonsunshine')
 def nature():
     return render_template('users/nature.html')
+
+@app.route('/post', methods =['POST', 'GET'])
+def post():
+    if request.method == 'POST':
+        f = request.files['datafile']
+        f.save('static/uploads/upload.png')
+        return "file uploaded" 
+
+    else:
+        return render_template('postPROTO.html')
+
+@app.route('/post/display')
+def display():
+    return render_template('postPROTO_POSTED.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
